1e2fa7628cc383de541fa5d848929d75cbde2908
DESTRUY PIERRE ERIC <destruy@me.com>
2022-04-21 13:45:42 +0200

    Modification HomeControler pour ajout d'un user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    public function index(): Response
     {
-        $user = new  User();
-        $user->setEmail('test@test.com');
-        $user->setPassword($userPasswordHasher->hashPassword($user,'123456'));
-        $user->setName('Doe');
-        $user->setActive(true);
-        $entityManager->persist($user);
-        $entityManager->flush();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
